0a10526a8d74e86a3e55b0c5988543ad415b2d51
Phantomhive123 <949805411@qq.com>
2019-12-13 23:55:54 +0800

    建立角色控制

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float MoveFacter = 1;
+    [SerializeField]
+    private float RotateFacter = 1;
+    [SerializeField]
+    private float RadiusOfSight = 0f;
+    [SerializeField][Range(1,5)]
+    private float LengthLightTimes = 2f;
+
+    private float h = 0f;
+    private float v = 0f;
+    private new Transform transform;
+    private bool JButton = false;
+    private bool KButton = false;
+
+    [SerializeField]
+    private Transform Scope = null;
+    [SerializeField]
+    private Transform Children = null;
+
+
+    private void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, v, 0) * MoveFacter * Time.deltaTime);
+    }
+
+    private void Rotate()
+    {
+        JButton = Input.GetKey(KeyCode.J);
+        KButton = Input.GetKey(KeyCode.K);
+
+        if (JButton ^ KButton)
+        {
+            Vector3 euler = JButton ? Vector3.forward : Vector3.back;
+            Scope.Rotate(euler * RotateFacter);
+        }
+    }
+}
---
+++ Assets/Scripts/ScopeTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ScopeTrigger : MonoBehaviour
+{
+    [NonSerialized]
+    public Vector3 ScaleOfScope = Vector3.zero;
+    [SerializeField]
+    private Transform Children = null;
+    [SerializeField]
+    private GameObject childPref = null;
+    [SerializeField]
+    private float increment = 2f;
+    private CircleCollider2D collider = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScaleOfScope = transform.localScale;
+        collider = GetComponent<CircleCollider2D>();
+    }
+
+    private void UpdateScope()
+    {
+        transform.localScale = ScaleOfScope;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CircleCollider2D other = collision.gameObject.GetComponent<CircleCollider2D>();
+        //判断是黑球还是白球
+        GameObject obj = Instantiate(childPref, Children);
+        Vector2 v = UnityEngine.Random.insideUnitCircle * (transform.localScale * collider.radius - other.radius * other.transform.localScale);
+        obj.transform.localPosition = v;
+        Destroy(other.gameObject);
+        //扩展
+        Expand();
+    }
+
+    private void Expand()
+    {
+        Vector3 newScale = new Vector3(transform.localScale.x + increment, transform.localScale.y + increment, transform.localScale.z);
+        transform.localScale = newScale;
+    }
+}
